55e7e4e9627b1e4b8438eff275581a0e99872ca8
Tom Shiflet <tom.shiflet@mylcs.com>
2019-12-25 12:53:58 -0600

    Initial Commit

---
+++ DungeonGen02/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DungeonGen02
+//
+//  Created by Tom Shiflet on 12/25/19.
+//  Copyright © 2019 LCS Game Design. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
